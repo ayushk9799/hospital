e7b0bd51b000850e435f5664bcf63744773ea23b
rajivranjan0013@gmail.com <rajivranjan0013@gmail.com>
2024-10-26 13:39:34 +0530

    some changes

--- frontend/src/assets/Data.js
+++ frontend/src/assets/Data.js
@@ -15,7 +15,7 @@ import {
 import { cn } from "../lib/utils";
 
 // backend url
-export const Backend_URL = "https://thehospital.in";
+export const Backend_URL = "http://192.168.1.4:3000";
 
 export const formatDate = (dateString) => {
   if (!dateString) return "N/A";
