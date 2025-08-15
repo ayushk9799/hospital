02e17bc16ce12b3e58acb2bdee5f6b0a30bf0216
Ayush Kumar <ayushk2.ug19.cs@nitp.ac.in>
2025-08-15 23:35:31 +0530

    chnages

--- backend/s3.js
+++ backend/s3.js
@@ -16,7 +16,7 @@ export const presignedUrl = async () => {
 
   const key = v4();
   const ObjectParams = {
-    Bucket: "thousandwayshospital",
+    Bucket: "mybucketthousand",
     Key: key,
   };
   const expiresIn = 3600;
